59e19e558ec8823cde84d826635876dfc22a2935
aingaingaing <3166537621@qq.com>
2025-11-10 20:07:55 +0800

    设备篇

---
+++ beveragemachine-manage/src/main/java/com/beveragemachine/manage/service/impl/VendingMachineServiceImpl.java
@@ -0,0 +1,96 @@
+package com.beveragemachine.manage.service.impl;
+
+import java.util.List;
+import com.beveragemachine.common.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.beveragemachine.manage.mapper.VendingMachineMapper;
+import com.beveragemachine.manage.domain.VendingMachine;
+import com.beveragemachine.manage.service.IVendingMachineService;
+
+/**
+ * 设备管理Service业务层处理
+ * 
+ * @author aing
+ * @date 2025-11-10
+ */
+@Service
+public class VendingMachineServiceImpl implements IVendingMachineService 
+{
+    @Autowired
+    private VendingMachineMapper vendingMachineMapper;
+
+    /**
+     * 查询设备管理
+     * 
+     * @param id 设备管理主键
+     * @return 设备管理
+     */
+    @Override
+    public VendingMachine selectVendingMachineById(Long id)
+    {
+        return vendingMachineMapper.selectVendingMachineById(id);
+    }
+
+    /**
+     * 查询设备管理列表
+     * 
+     * @param vendingMachine 设备管理
+     * @return 设备管理
+     */
+    @Override
+    public List<VendingMachine> selectVendingMachineList(VendingMachine vendingMachine)
+    {
+        return vendingMachineMapper.selectVendingMachineList(vendingMachine);
+    }
+
+    /**
+     * 新增设备管理
+     * 
+     * @param vendingMachine 设备管理
+     * @return 结果
+     */
+    @Override
+    public int insertVendingMachine(VendingMachine vendingMachine)
+    {
+        vendingMachine.setCreateTime(DateUtils.getNowDate());
+        return vendingMachineMapper.insertVendingMachine(vendingMachine);
+    }
+
+    /**
+     * 修改设备管理
+     * 
+     * @param vendingMachine 设备管理
+     * @return 结果
+     */
+    @Override
+    public int updateVendingMachine(VendingMachine vendingMachine)
+    {
+        vendingMachine.setUpdateTime(DateUtils.getNowDate());
+        return vendingMachineMapper.updateVendingMachine(vendingMachine);
+    }
+
+    /**
+     * 批量删除设备管理
+     * 
+     * @param ids 需要删除的设备管理主键
+     * @return 结果
+     */
+    @Override
+    public int deleteVendingMachineByIds(Long[] ids)
+    {
+        return vendingMachineMapper.deleteVendingMachineByIds(ids);
+    }
+
+    /**
+     * 删除设备管理信息
+     * 
+     * @param id 设备管理主键
+     * @return 结果
+     */
+    @Override
+    public int deleteVendingMachineById(Long id)
+    {
+        return vendingMachineMapper.deleteVendingMachineById(id);
+    }
+}
